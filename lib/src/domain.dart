3f09e07b9ff12b2cfd851c8f6fe68dde95f90461
phantamanta44 <evandalong@gmail.com>
2017-01-09 19:21:28 -0600

    Use regexps for domains

--- lib/src/domain.dart
+++ lib/src/domain.dart
@@ -1,36 +1,36 @@
 part of ws_server;
 
 final Domain domainAcceptAll = new AcceptAllDomain();
-final RegExp spaceRegexp = new RegExp(r'\s+');
 
 abstract class Domain {
-  List<String> get list;
+  List<Pattern> get list;
 
   bool get isBlacklist;
 
   bool allowed(String input) =>
-      isBlacklist != list.contains(input.trim().toLowerCase());
+      isBlacklist != list.any((p) => p.matchAsPrefix(input) != null);
 
   @override
-  String toString() => '${isBlacklist ? '!' : '-'}${list.join(' ')}';
+  String toString() => '${isBlacklist ? '!' : '-'}${list.join(r'\|')}';
 }
 
 class AcceptAllDomain extends Domain {
   @override
-  List<String> get list => [];
+  List<Pattern> get list => [];
 
   @override
   bool get isBlacklist => true;
 }
 
 class DeserializedDomain extends Domain {
-  List<String> list;
+  List<Pattern> list;
   bool isBlacklist;
 
   DeserializedDomain() : this.of([], false);
 
   DeserializedDomain.of(this.list, this.isBlacklist);
 
-  DeserializedDomain.from(String domain)
-      : this.of(domain.substring(1).split(spaceRegexp), domain.startsWith('!'));
+  DeserializedDomain.from(String domain) : this.of(
+      domain.substring(1).split(r'\|').map((p) => new RegExp(p)),
+      domain.startsWith('!'));
 }
